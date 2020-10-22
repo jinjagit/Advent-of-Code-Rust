68b11c74197553962b9acb66eed7e6547a0f31e3
Simon Tharby <jinjamail@gmail.com>
2020-10-22 16:23:24 +0100

    Ex. 2a correct solution

--- exercise2a/src/main.rs
+++ exercise2a/src/main.rs
@@ -1,5 +1,15 @@
 fn main() {
-    let intcode = vec![1, 1, 1, 4, 2, 5, 6, 0, 99];
+    let mut intcode = vec![
+        1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 10, 1, 19, 2, 19, 6, 23, 2, 13, 23, 27,
+        1, 9, 27, 31, 2, 31, 9, 35, 1, 6, 35, 39, 2, 10, 39, 43, 1, 5, 43, 47, 1, 5, 47, 51, 2, 51,
+        6, 55, 2, 10, 55, 59, 1, 59, 9, 63, 2, 13, 63, 67, 1, 10, 67, 71, 1, 71, 5, 75, 1, 75, 6,
+        79, 1, 10, 79, 83, 1, 5, 83, 87, 1, 5, 87, 91, 2, 91, 6, 95, 2, 6, 95, 99, 2, 10, 99, 103,
+        1, 103, 5, 107, 1, 2, 107, 111, 1, 6, 111, 0, 99, 2, 14, 0, 0,
+    ];
+
+    // "... replace position 1 with the value 12 and replace position 2 with the value 2."
+    intcode[1] = 12;
+    intcode[2] = 2;
 
     let result = run_all_intcode_blocks(&intcode);
 
@@ -18,9 +28,6 @@ fn run_all_intcode_blocks(intcode: &Vec<u32>) -> Vec<u32> {
             break;
         } else if let Some(_value) = &intcode.get(start) {
             let code_block = &intcode[start..start + 4];
-
-            println!("block: {:?}", code_block);
-
             new_intcode = run_intcode_block(&new_intcode, code_block);
         } else {
             println!("Error! No exit code '99' encountered");
@@ -52,20 +59,20 @@ mod tests {
     use super::*;
     #[test]
     fn run_all_intcode_blocks_test() {
-        let intcode = vec![1,0,0,0,99];
+        let intcode = vec![1, 0, 0, 0, 99];
         let result = run_all_intcode_blocks(&intcode);
-        assert_eq!(result, [2,0,0,0,99]);
+        assert_eq!(result, [2, 0, 0, 0, 99]);
 
-        let intcode = vec![2,3,0,3,99];
+        let intcode = vec![2, 3, 0, 3, 99];
         let result = run_all_intcode_blocks(&intcode);
-        assert_eq!(result, [2,3,0,6,99]);
+        assert_eq!(result, [2, 3, 0, 6, 99]);
 
-        let intcode = vec![2,4,4,5,99,0];
+        let intcode = vec![2, 4, 4, 5, 99, 0];
         let result = run_all_intcode_blocks(&intcode);
-        assert_eq!(result, [2,4,4,5,99,9801]);
+        assert_eq!(result, [2, 4, 4, 5, 99, 9801]);
 
         let intcode = vec![1, 1, 1, 4, 2, 5, 6, 0, 99];
         let result = run_all_intcode_blocks(&intcode);
-        assert_eq!(result, [30,1,1,4,2,5,6,0,99]);
+        assert_eq!(result, [30, 1, 1, 4, 2, 5, 6, 0, 99]);
     }
 }
