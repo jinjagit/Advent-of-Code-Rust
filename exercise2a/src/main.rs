72f6872657fea1a945201778b7b96fdc9e11c01a
Simon Tharby <jinjamail@gmail.com>
2020-10-22 15:13:27 +0100

    pass to, and return mutated version of vec from, fn

--- exercise2a/src/main.rs
+++ exercise2a/src/main.rs
@@ -7,22 +7,30 @@ fn main() {
 }
 
 
-fn run_intcode_block(intcode: &Vec<u32>, code_block: &[u32]) {
-    let intcode_iter = intcode.iter();
+fn run_intcode_block(intcode: &Vec<u32>, code_block: &[u32]) -> Vec<u32> {
+    //let new_intcode = &intcode;
 
-    for i in intcode_iter {
-        println!("i: {}", i);
-    }
+    // for i in intcode_iter {
+    //     println!("i: {}", i);
+    // }
+
+    let mut new_intcode = intcode.to_vec();
+
+
 
     for i in 0..4 {
         println!("code_block i: {} = {}", i, code_block[i]);
     }
+
+    new_intcode[2] = 777;
+
+    new_intcode
 }
 
 fn run_all_intcode_blocks(intcode: &Vec<u32>) {
     let mut counter: usize = 0;
 
-    let mut new_intcode = &intcode;
+    let mut new_intcode = intcode.to_vec();
 
     loop {
         let start = counter * 4;
@@ -31,7 +39,7 @@ fn run_all_intcode_blocks(intcode: &Vec<u32>) {
             break;
         } else if let Some(_value) = &intcode.get(start) {
             let code_block = &intcode[start..start + 4];
-            run_intcode_block(new_intcode, code_block);
+            new_intcode = run_intcode_block(&new_intcode, code_block);
         } else {
             println!("Error! No exit code '99' encountered");
             break;
@@ -39,4 +47,24 @@ fn run_all_intcode_blocks(intcode: &Vec<u32>) {
 
         counter += 1;
     }
+
+    println!("mutated intcode: {:?}", new_intcode);
 }
+
+// fn main() {
+//     let mut my_vec = vec![1, 2, 3];
+//
+//     my_vec = mutate_vec(&my_vec);
+//
+//     println!("my_vec: {:?}", my_vec);
+// }
+//
+// fn mutate_vec(vec: &Vec<u32>) -> Vec<u32> {
+//     println!("vec: {:?}", vec);
+//
+//     let mut new_vec = vec.to_vec();
+//
+//     new_vec[2] = 777;
+//
+//     new_vec
+// }
