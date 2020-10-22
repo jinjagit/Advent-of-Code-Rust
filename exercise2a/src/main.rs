ad05bb48cc8c86f0dd4477fa8752429cdfab352a
Simon Tharby <jinjamail@gmail.com>
2020-10-22 14:16:27 +0100

    break if exit code 99 encountered

--- exercise2a/src/main.rs
+++ exercise2a/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let intcode = vec![1, 2, 3, 4, 5, 6, 7 , 8];
+    let intcode = vec![1, 2, 3, 4, 5, 6, 7 , 8, 99, 6, 6, 6];
 
     run_all_intcode_blocks(&intcode);
 
@@ -22,13 +22,18 @@ fn run_intcode_block(intcode: &Vec<u32>, code_block: &[u32]) {
 fn run_all_intcode_blocks(intcode: &Vec<u32>) {
     let mut counter: usize = 0;
 
+    let mut new_intcode = &intcode;
+
     loop {
         let start = counter * 4;
 
-        if let Some(_value) = &intcode.get(start) {
+        if let Some(99) = &intcode.get(start) {
+            break;
+        } else if let Some(_value) = &intcode.get(start) {
             let code_block = &intcode[start..start + 4];
-            run_intcode_block(intcode, code_block);
+            run_intcode_block(new_intcode, code_block);
         } else {
+            println!("Error! No exit code '99' encountered");
             break;
         }
 
