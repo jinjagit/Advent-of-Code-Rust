b943f6b5ba23adb72dffb45bd7f560c8dbcbb4a9
Simon Tharby <jinjamail@gmail.com>
2020-10-22 20:01:46 +0100

    Ex. 2a refactored solution

--- exercise2a/src/main.rs
+++ exercise2a/src/main.rs
@@ -1,9 +1,15 @@
 fn main() {
-    let mut intcode = vec![1, 0, 0, 0, 99];
+    let mut intcode = vec![
+        1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 10, 1, 19, 2, 19, 6, 23, 2, 13, 23, 27,
+        1, 9, 27, 31, 2, 31, 9, 35, 1, 6, 35, 39, 2, 10, 39, 43, 1, 5, 43, 47, 1, 5, 47, 51, 2, 51,
+        6, 55, 2, 10, 55, 59, 1, 59, 9, 63, 2, 13, 63, 67, 1, 10, 67, 71, 1, 71, 5, 75, 1, 75, 6,
+        79, 1, 10, 79, 83, 1, 5, 83, 87, 1, 5, 87, 91, 2, 91, 6, 95, 2, 6, 95, 99, 2, 10, 99, 103,
+        1, 103, 5, 107, 1, 2, 107, 111, 1, 6, 111, 0, 99, 2, 14, 0, 0,
+    ];
 
     // "... replace position 1 with the value 12 and replace position 2 with the value 2."
-    // intcode[1] = 12;
-    // intcode[2] = 2;
+    intcode[1] = 12;
+    intcode[2] = 2;
 
     let result = run_all_intcode_blocks(&intcode);
 
@@ -46,8 +52,6 @@ fn run_intcode_block(intcode: &Vec<u32>, start: &usize) -> Vec<u32> {
     } else if new_intcode[*start] == 2 {
         new_intcode[result_index] = new_intcode[input_a_index] * new_intcode[input_b_index];
     }
-
-    println!("4th value: {}", new_intcode[*start + 3]);
 
     new_intcode
 }
