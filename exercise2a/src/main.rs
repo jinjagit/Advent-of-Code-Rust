e77e247d767d74a8279d2e9b0f5d14297b6954c4
Simon Tharby <jinjamail@gmail.com>
2020-10-22 16:18:47 +0100

    Ex. 2a all tests passing

--- exercise2a/src/main.rs
+++ exercise2a/src/main.rs
@@ -1,33 +1,12 @@
 fn main() {
-    let intcode = vec![1, 2, 3, 4, 5, 6, 7 , 8, 99, 6, 6, 6];
+    let intcode = vec![1, 1, 1, 4, 2, 5, 6, 0, 99];
 
-    run_all_intcode_blocks(&intcode);
+    let result = run_all_intcode_blocks(&intcode);
 
-    println!("intcode in main: {:?}", intcode);
+    println!("result: {:?}", result);
 }
 
-
-fn run_intcode_block(intcode: &Vec<u32>, code_block: &[u32]) -> Vec<u32> {
-    //let new_intcode = &intcode;
-
-    // for i in intcode_iter {
-    //     println!("i: {}", i);
-    // }
-
-    let mut new_intcode = intcode.to_vec();
-
-
-
-    for i in 0..4 {
-        println!("code_block i: {} = {}", i, code_block[i]);
-    }
-
-    new_intcode[2] = 777;
-
-    new_intcode
-}
-
-fn run_all_intcode_blocks(intcode: &Vec<u32>) {
+fn run_all_intcode_blocks(intcode: &Vec<u32>) -> Vec<u32> {
     let mut counter: usize = 0;
 
     let mut new_intcode = intcode.to_vec();
@@ -39,6 +18,9 @@ fn run_all_intcode_blocks(intcode: &Vec<u32>) {
             break;
         } else if let Some(_value) = &intcode.get(start) {
             let code_block = &intcode[start..start + 4];
+
+            println!("block: {:?}", code_block);
+
             new_intcode = run_intcode_block(&new_intcode, code_block);
         } else {
             println!("Error! No exit code '99' encountered");
@@ -48,23 +30,42 @@ fn run_all_intcode_blocks(intcode: &Vec<u32>) {
         counter += 1;
     }
 
-    println!("mutated intcode: {:?}", new_intcode);
+    new_intcode
 }
 
-// fn main() {
-//     let mut my_vec = vec![1, 2, 3];
-//
-//     my_vec = mutate_vec(&my_vec);
-//
-//     println!("my_vec: {:?}", my_vec);
-// }
-//
-// fn mutate_vec(vec: &Vec<u32>) -> Vec<u32> {
-//     println!("vec: {:?}", vec);
-//
-//     let mut new_vec = vec.to_vec();
-//
-//     new_vec[2] = 777;
-//
-//     new_vec
-// }
+fn run_intcode_block(intcode: &Vec<u32>, code_block: &[u32]) -> Vec<u32> {
+    let mut new_intcode = intcode.to_vec();
+
+    if code_block[0] == 1 {
+        new_intcode[code_block[3] as usize] =
+            new_intcode[code_block[1] as usize] + new_intcode[code_block[2] as usize];
+    } else if code_block[0] == 2 {
+        new_intcode[code_block[3] as usize] =
+            new_intcode[code_block[1] as usize] * new_intcode[code_block[2] as usize];
+    }
+
+    new_intcode
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn run_all_intcode_blocks_test() {
+        let intcode = vec![1,0,0,0,99];
+        let result = run_all_intcode_blocks(&intcode);
+        assert_eq!(result, [2,0,0,0,99]);
+
+        let intcode = vec![2,3,0,3,99];
+        let result = run_all_intcode_blocks(&intcode);
+        assert_eq!(result, [2,3,0,6,99]);
+
+        let intcode = vec![2,4,4,5,99,0];
+        let result = run_all_intcode_blocks(&intcode);
+        assert_eq!(result, [2,4,4,5,99,9801]);
+
+        let intcode = vec![1, 1, 1, 4, 2, 5, 6, 0, 99];
+        let result = run_all_intcode_blocks(&intcode);
+        assert_eq!(result, [30,1,1,4,2,5,6,0,99]);
+    }
+}
