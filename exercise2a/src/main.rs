dac9b631a6aac81195d192601134359300b6743a
Simon Tharby <jinjamail@gmail.com>
2020-10-22 13:16:13 +0100

    basic slicing of vec and passing ref to vec

--- exercise2a/src/main.rs
+++ exercise2a/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let intcode = vec![1, 2, 3, 4, 5, 6, 7 , 8];
+
+    let slice = &intcode[0..4];
+
+    run_intcode_block(&intcode);
+
+    println!("intcode: {:?}", intcode);
+    println!("slice: {:?}", slice);
+    println!("slice[0]: {:?}", slice[0]);
+}
+
+fn run_intcode_block(intcode: &Vec<u32>) {
+    let intcode_iter = intcode.iter();
+
+    for i in intcode_iter {
+        println!("i: {}", i);
+    }
 }
