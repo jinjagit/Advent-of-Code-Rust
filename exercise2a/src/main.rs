0aa859ed243cd0280136775ba63413fe87c2dd82
Simon Tharby <jinjamail@gmail.com>
2020-10-22 13:39:10 +0100

    pass ref to vec slice to fn

--- exercise2a/src/main.rs
+++ exercise2a/src/main.rs
@@ -1,19 +1,29 @@
 fn main() {
     let intcode = vec![1, 2, 3, 4, 5, 6, 7 , 8];
 
-    let slice = &intcode[0..4];
+    run_all_code_blocks(&intcode);
 
-    run_intcode_block(&intcode);
-
-    println!("intcode: {:?}", intcode);
-    println!("slice: {:?}", slice);
-    println!("slice[0]: {:?}", slice[0]);
+    println!("intcode in main: {:?}", intcode);
 }
 
-fn run_intcode_block(intcode: &Vec<u32>) {
+
+fn run_intcode_block(intcode: &Vec<u32>, code_block: &[u32]) {
     let intcode_iter = intcode.iter();
 
     for i in intcode_iter {
         println!("i: {}", i);
     }
+
+    for i in 0..4 {
+        println!("code_block i: {} = {}", i, code_block[i]);
+    }
+}
+
+fn run_all_code_blocks(intcode: &Vec<u32>) {
+    let code_block = &intcode[0..4];
+    
+    println!("slice: {:?}", code_block);
+    println!("slice[0]: {:?}", code_block[0]);
+
+    run_intcode_block(intcode, code_block);
 }
