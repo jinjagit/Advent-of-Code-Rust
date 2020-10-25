431ab29894ec8b034f9c40b7889cd14ec083d6dc
Simon Tharby <jinjamail@gmail.com>
2020-10-25 20:36:11 +0000

    Ex 5a. working solution

--- exercise5a/README.md
+++ exercise5a/README.md
@@ -1,4 +1,4 @@
---- Day 5: Sunny with a Chance of Asteroids ---  
+# Day 5: Sunny with a Chance of Asteroids 
   
 You're starting to sweat as the ship makes its way toward Mercury. The Elves suggest that you get the air conditioner working by upgrading your ship computer to support the Thermal Environment Supervision Terminal.  
   
@@ -50,3 +50,79 @@ Finally, the program will output a diagnostic code and immediately halt. This fi
 After providing 1 to the only input instruction and passing all the tests, what diagnostic code does the program produce?  
   
 To begin, get your puzzle input.  
+
+------------------------------------------------------------------------------------------------  
+
+# My notes:
+
+Summary of ship computer functionality:  
+  
+code executed in blocks (no longer always of 4 elements)  
+1st instruction contains opcode (as last 1 or 2 digits):  
+  
+opcodes:  
+01 = add 2 parameters & return result (write to position in memory vec)  
+02 = multiply 2 parameters & return result (write to position in memory vec)  
+03 = save an input parameter (write to position in memory vec)  
+04 = output a parameter (write to outputs vec)  
+99 = end program  
+
+First instruction:  
+ABCDE  
+ 1002  
+
+DE - two-digit opcode,      02 == opcode 2  
+ C - mode of 1st parameter,  0 == position mode  
+ B - mode of 2nd parameter,  1 == immediate mode  
+ A - mode of 3rd parameter,  0 == position mode,  
+                                  omitted due to being a leading zero  
+
+parameter modes:  
+0 = position (parameter is interpreted as a value)  
+1 = immediate (parameter is interpreted as a value)  
+  
+-------------------------------------------------------------------------------------------------  
+Parameters that an instruction writes to will never be in immediate mode.    
+  What does this mean?  
+  I think we can consider opcodes 01 & 02 as ALWAYS writing to a parameter.  
+  Thus, their 3rd parameter will always be position mode (0).  
+  
+xyz01, 1, 2, 2 => opcode 1, R, R, W (x will not be immediate mode == position == 0)  
+R's (reads) could be immediate values or values at positions (depending on parameter modes z & y)  
+
+I am also assuming that there will only be one input instruction encountered, or I am confused!  
+If this is true, then we do not consider output as 'writing', but reading (either a position or
+an immediate value).  
+Input probably must be in position mode (it's 'given' a value to put somewhere in 'memory').  
+-------------------------------------------------------------------------------------------------  
+  
+It is important to remember that the instruction pointer should increase by the number of values  
+in the instruction after the instruction finishes. Because of the new instructions,
+this amount is no longer always 4. (opcodes 3 & 4 result in + 1 value? = + 2 pointer?)  
+  
+Integers can be negative: 1101,100,-1,4,0 is a valid program  
+(find 100 + -1, store the result in position 4).  
+  
+The program 3,0,4,0,99 outputs whatever it gets as input, then halts.  
+i.e. opcode 03 saves whatever it gets as input at position 0, opcode 04 (3rd element) ouputs
+whatever has been given as an input, at position 0. Opcode 99 then terminates the program.  
+  
+If all outputs were zero except the diagnostic code, the diagnostic program ran successfully.
+Need a check (boolean?) for all outputs except ultimate being zero.  
+(all not immediately followed by '99' exit code)  
+if memory[i] is 4 (or ends in 04) && memory[i + 2] != 99, then output value must be zero.  
+Or could just store all ouptuts and print when 'program' exits.  
+  
+Final output = value to enter as solution.  
+  
+Plan:  
+
+1. Parse instruction code  
+   * could be 1 to 5 digits  
+   * opcode = last 2 digits, or 1 if only 1 digit  
+   * absent parameter modes = 0  
+   * different opcodes require different numbers of parameters  
+      thus (re)set 3 param modes to zero (param_1_mode = 0, etc.)  
+      thus parse what is given and store modes (param_1_mode = 1, etc.)  
+      then each opcode can access the number of param mode values it needs  
+      maybe use a struct for this?  
--- exercise5a/src/main.rs
+++ exercise5a/src/main.rs
@@ -1,75 +1,3 @@
-// Summary of ship computer functionality:
-
-// code executed in blocks (no longer always of 4 elements)
-// 1st instruction contains opcode (as last 2 digits):
-
-// opcodes:
-// 01 = add 2 parameters & return result
-// 02 = multiply 2 parameters & return result
-// 03 = save an input parameter
-// 04 = output a parameter
-// 99 = end program
-
-// First instruction:
-// ABCDE
-//  1002
-
-// DE - two-digit opcode,      02 == opcode 2
-//  C - mode of 1st parameter,  0 == position mode
-//  B - mode of 2nd parameter,  1 == immediate mode
-//  A - mode of 3rd parameter,  0 == position mode,
-//                                   omitted due to being a leading zero
-
-// parameter modes:
-// 0 = position (parameter is interpreted as a value)
-// 1 = immediate (parameter is interpreted as a value)
-
-// -------------------------------------------------------------------------------------------------
-// Parameters that an instruction writes to will never be in immediate mode.
-//   What does this mean?
-//   I think we can consider opcodes 01 & 02 as ALWAYS writing to a parameter.
-//   Thus, their 3rd parameter will always be position mode (0).
-
-// xyz01, 1, 2, 2 => opcode 1, R, R, W (x will not be immediate mode == position == 0)
-// R's (reads) could be immediate values or values at positions (depending on parameter modes z & y)
-
-// I am also assuming that there will only be one input instruction encountered, or I am confused!
-// If this is true, then we do not consider output as 'writing', but reading (either a position or
-// an immediate value).
-// Input probably must be in position mode (it's 'given' a value to put somewhere in 'memory').
-// -------------------------------------------------------------------------------------------------
-
-// It is important to remember that the instruction pointer should increase by the number of values
-// in the instruction after the instruction finishes. Because of the new instructions,
-// this amount is no longer always 4. (opcodes 3 & 4 result in + 1 value? = + 2 pointer?)
-//
-// Integers can be negative: 1101,100,-1,4,0 is a valid program
-// (find 100 + -1, store the result in position 4).
-
-// The program 3,0,4,0,99 outputs whatever it gets as input, then halts.
-// i.e. opcode 03 saves whatever it gets as input at position 0, opcode 04 (3rd element) ouputs
-// whatever has been given as an input, at position 0. Opcode 99 then terminates the program.
-
-// If all outputs were zero except the diagnostic code, the diagnostic program ran successfully.
-// Need a check (boolean?) for all outputs except ultimate being zero.
-// (all not immediately followed by '99' exit code)
-// if memory[i] is 4 (or ends in 04) && memory[i + 2] != 99, then output value must be zero.
-// Or could just store all ouptuts and print when 'program' exits.
-
-// Final output = value to enter as solution.
-
-// Plan:
-//
-// 1. Parse instruction code
-//    * could be 1 to 5 digits
-//    * opcode = last 2 digits, or 1 if only 1 digit
-//    * absent parameter modes = 0
-//    * different opcodes require different numbers of parameters
-//       thus (re)set 3 param modes to zero (param_1_mode = 0, etc.)
-//       thus parse what is given and store modes (param_1_mode = 1, etc.)
-//       then each opcode can access the number of param mode values it needs
-//       maybe use a struct for this?
-
 use std::fs;
 
 pub struct InstructionSet {
@@ -142,16 +70,49 @@ impl InstructionSet {
 }
 fn main() {
     let mut memory: Vec<i32> = parse_memory_from_text_file("input.txt");
+    let mut pointer: usize = 0;
     let mut intcode: InstructionSet = Default::default();
+    let mut outputs: Vec<i32> = vec![];
+    let input: i32 = 1;
 
-    println!("{:?}", memory);
+    loop {
+        intcode.new_raw_code(memory[pointer]);
 
-    // intcode.new_raw_code(4);
+        if intcode.opcode == 1 {
+            let param_1 = get_value(&intcode.param1_mode, &memory[pointer + 1], &memory);
+            let param_2 = get_value(&intcode.param2_mode, &memory[pointer + 2], &memory);
+            let address = memory[pointer + 3] as usize;
+            memory[address] = param_1 + param_2;
+            pointer += 4;
+        } else if intcode.opcode == 2 {
+            let param_1 = get_value(&intcode.param1_mode, &memory[pointer + 1], &memory);
+            let param_2 = get_value(&intcode.param2_mode, &memory[pointer + 2], &memory);
+            let address = memory[pointer + 3] as usize;
+            memory[address] = param_1 * param_2;
+            pointer += 4;
+        } else if intcode.opcode == 3 {
+            let address = memory[pointer + 1] as usize;
+            memory[address] = input;
+            pointer += 2;
+        } else if intcode.opcode == 4 {
+            let value: i32 = get_value(&intcode.param1_mode, &memory[pointer + 1], &memory);
+            outputs.push(value);
+            pointer += 2;
+        } else if intcode.opcode == 99 {
+            break;
+        }
+    }
 
-    // println!(
-    //     "opcode: {} - modes; p1: {}, p2: {}, p3: {}",
-    //     intcode.opcode, intcode.param1_mode, intcode.param2_mode, intcode.param3_mode
-    // );
+    println!("outputs: {:?}", outputs);
+}
+
+fn get_value(param_mode: &u8, val_or_posn: &i32, memory: &Vec<i32>) -> i32 {
+    if param_mode == &0 {
+        let position = *val_or_posn as usize;
+        return memory[position] as i32;
+    } else {
+        return *val_or_posn;
+    }
 }
 
 fn parse_memory_from_text_file(filename: &str) -> Vec<i32> {
