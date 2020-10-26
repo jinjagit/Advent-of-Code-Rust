337f2ba04d4605e27df9daea430a857982c91755
Simon Tharby <jinjamail@gmail.com>
2020-10-26 22:15:29 +0000

    Ex. 5b working solution, but needs refactoring to reduce repetition

--- exercise5b/src/main.rs
+++ exercise5b/src/main.rs
@@ -35,9 +35,9 @@ impl InstructionSet {
         let count: usize = digits.iter().count();
 
         // Parse opcode.
-        if count == 1 && (digits[0] > 0 && digits[0] < 5) {
+        if count == 1 && (digits[0] > 0 && digits[0] < 9) {
             self.opcode = digits[0];
-        } else if digits[count - 2] == 0 && (digits[count - 1] > 0 && digits[count - 1] < 5) {
+        } else if digits[count - 2] == 0 && (digits[count - 1] > 0 && digits[count - 1] < 9) {
             self.opcode = digits[count - 1];
         } else if digits[count - 2] == 9 && digits[count - 1] == 9 {
             self.opcode = 99;
@@ -71,7 +71,7 @@ impl InstructionSet {
 
 fn main() {
     let memory: Vec<i32> = parse_memory_from_text_file("input.txt");
-    let input: i32 = 1;
+    let input: i32 = 5;
 
     let outputs = run_program(memory, input);
 
@@ -90,22 +90,66 @@ fn run_program(mut memory: Vec<i32>, input: i32) -> Vec<i32> {
             let param_1 = get_value(&intcode.param1_mode, &memory[pointer + 1], &memory);
             let param_2 = get_value(&intcode.param2_mode, &memory[pointer + 2], &memory);
             let address = memory[pointer + 3] as usize;
+
             memory[address] = param_1 + param_2;
             pointer += 4;
         } else if intcode.opcode == 2 {
             let param_1 = get_value(&intcode.param1_mode, &memory[pointer + 1], &memory);
             let param_2 = get_value(&intcode.param2_mode, &memory[pointer + 2], &memory);
             let address = memory[pointer + 3] as usize;
+
             memory[address] = param_1 * param_2;
             pointer += 4;
         } else if intcode.opcode == 3 {
             let address = memory[pointer + 1] as usize;
+
             memory[address] = input;
             pointer += 2;
         } else if intcode.opcode == 4 {
             let value: i32 = get_value(&intcode.param1_mode, &memory[pointer + 1], &memory);
+
             outputs.push(value);
             pointer += 2;
+        } else if intcode.opcode == 5 {
+            let param_1 = get_value(&intcode.param1_mode, &memory[pointer + 1], &memory);
+
+            if param_1 != 0 {
+                let param_2 = get_value(&intcode.param2_mode, &memory[pointer + 2], &memory);
+                pointer = param_2 as usize;
+            } else {
+                pointer += 3
+            }
+        } else if intcode.opcode == 6 {
+            let param_1 = get_value(&intcode.param1_mode, &memory[pointer + 1], &memory);
+
+            if param_1 == 0 {
+                let param_2 = get_value(&intcode.param2_mode, &memory[pointer + 2], &memory);
+                pointer = param_2 as usize;
+            } else {
+                pointer += 3
+            }
+        } else if intcode.opcode == 7 {
+            let param_1 = get_value(&intcode.param1_mode, &memory[pointer + 1], &memory);
+            let param_2 = get_value(&intcode.param2_mode, &memory[pointer + 2], &memory);
+            let address = memory[pointer + 3] as usize;
+
+            if param_1 < param_2 {
+                memory[address] = 1;
+            } else {
+                memory[address] = 0;
+            }
+            pointer += 4;
+        } else if intcode.opcode == 8 {
+            let param_1 = get_value(&intcode.param1_mode, &memory[pointer + 1], &memory);
+            let param_2 = get_value(&intcode.param2_mode, &memory[pointer + 2], &memory);
+            let address = memory[pointer + 3] as usize;
+
+            if param_1 == param_2 {
+                memory[address] = 1;
+            } else {
+                memory[address] = 0;
+            }
+            pointer += 4;
         } else if intcode.opcode == 99 {
             break;
         }
@@ -194,20 +238,75 @@ mod tests {
 
     #[test]
     fn run_program_test() {
-        // Tests of examples & exercise from 5a:
-        let outputs = run_program(vec![3, 0, 4, 0, 99], 42);
-        assert_eq!(outputs, [42]);
+        // Tests of: is input == 8 ? (1 = true, 0 = false) - position mode
+        let outputs = run_program(vec![3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8], 8);
+        assert_eq!(outputs, [1]);
 
-        let outputs = run_program(vec![1002, 4, 3, 4, 33], 42);
-        assert_eq!(outputs, []);
+        let outputs = run_program(vec![3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8], 7);
+        assert_eq!(outputs, [0]);
 
-        let outputs = run_program(vec![1101, 100, -1, 4, 0], 42);
-        assert_eq!(outputs, []);
+        let outputs = run_program(vec![3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8], 9);
+        assert_eq!(outputs, [0]);
 
-        let memory: Vec<i32> = parse_memory_from_text_file("5a_input.txt");
-        let outputs = run_program(memory, 1);
-        assert_eq!(outputs, [0, 0, 0, 0, 0, 0, 0, 0, 0, 7286649]);
+        // Tests of: is input < 8 ? (1 = true, 0 = false) - position mode
+        let outputs = run_program(vec![3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8], 8);
+        assert_eq!(outputs, [0]);
 
-        // Tests of examples for this exercise:
+        let outputs = run_program(vec![3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8], 7);
+        assert_eq!(outputs, [1]);
+
+        let outputs = run_program(vec![3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8], 9);
+        assert_eq!(outputs, [0]);
+
+        // Tests of: is input == 8 ? (1 = true, 0 = false) - immediate mode
+        let outputs = run_program(vec![3, 3, 1108, -1, 8, 3, 4, 3, 99], 8);
+        assert_eq!(outputs, [1]);
+
+        let outputs = run_program(vec![3, 3, 1108, -1, 8, 3, 4, 3, 99], 7);
+        assert_eq!(outputs, [0]);
+
+        let outputs = run_program(vec![3, 3, 1108, -1, 8, 3, 4, 3, 99], 9);
+        assert_eq!(outputs, [0]);
+
+        // Tests of: is input < 8 ? (1 = true, 0 = false) - immediate mode
+        let outputs = run_program(vec![3, 3, 1107, -1, 8, 3, 4, 3, 99], 8);
+        assert_eq!(outputs, [0]);
+
+        let outputs = run_program(vec![3, 3, 1107, -1, 8, 3, 4, 3, 99], 7);
+        assert_eq!(outputs, [1]);
+
+        let outputs = run_program(vec![3, 3, 1107, -1, 8, 3, 4, 3, 99], 9);
+        assert_eq!(outputs, [0]);
+
+        // Tests of: is input == 8 || input < 8 || input > 8 ? (999 = less, 1000 = equal, 1001 = greater)
+        let outputs = run_program(
+            vec![
+                3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36,
+                98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000,
+                1, 20, 4, 20, 1105, 1, 46, 98, 99,
+            ],
+            8,
+        );
+        assert_eq!(outputs, [1000]);
+
+        let outputs = run_program(
+            vec![
+                3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36,
+                98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000,
+                1, 20, 4, 20, 1105, 1, 46, 98, 99,
+            ],
+            7,
+        );
+        assert_eq!(outputs, [999]);
+
+        let outputs = run_program(
+            vec![
+                3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36,
+                98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000,
+                1, 20, 4, 20, 1105, 1, 46, 98, 99,
+            ],
+            9,
+        );
+        assert_eq!(outputs, [1001]);
     }
 }
