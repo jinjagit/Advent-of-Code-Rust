a27432c80d9f38ffa736168cb8a98ee47e9514c5
Simon Tharby <jinjamail@gmail.com>
2020-10-27 20:56:22 +0000

    Ex. 6a create and read vec of structs

--- exercise6a/src/main.rs
+++ exercise6a/src/main.rs
@@ -1,3 +1,31 @@
+
+pub struct Body {
+    name: String,
+    orbits: i32,
+    orbiting: String,
+}
+
+impl Body {
+    fn print(&self) {
+        println!("name: {}, orbits: {}, orbiting: {}", self.name, self.orbits, self.orbiting);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut my_vec: Vec<Body> = vec![];
+
+    for i in 0..5 {
+        let my_struct = Body {
+            name: String::from("some_name"),
+            orbits: i,
+            orbiting: String::from("some_name"), 
+        };
+
+        my_vec.push(my_struct);
+    }
+
+    for elem in my_vec {
+        elem.print();
+    }
+    
 }
