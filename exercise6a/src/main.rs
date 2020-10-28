68917444780b2e1cb4ba4e048d18f5c2fa1a9c34
Simon Tharby <jinjamail@gmail.com>
2020-10-28 02:08:43 +0000

    Ex. 6a add dev comments

--- exercise6a/src/main.rs
+++ exercise6a/src/main.rs
@@ -7,7 +7,7 @@ pub struct Body<'a> {
 }
 
 impl Body<'_> {
-    fn print(&self) {
+    fn print(&self) { // DEBUG
         println!("name: {}, orbits: {}, orbiting: {}", self.name, self.orbits, self.orbiting);
     }
 
@@ -17,20 +17,24 @@ impl Body<'_> {
 }
 
 fn main() {
-    // get vec of components from input
+    // Get vec of names (&str's) of orbiting bodies from input text file.
     let input_string: String = fs::read_to_string("input.txt").expect("Error reading file!");
     let split_input: Vec<&str> = input_string.split('\n').collect();
     let mut input: Vec<&str> = vec![];
 
-    for elem in &split_input {
+    for elem in split_input {
         let mut new_elem: Vec<&str> = elem.split(")").collect();
         input.append(&mut new_elem);
     }
 
     println!("input vec: {:?}", input); // DEBUG
 
-    // next step (in development): produce vec of structs from input vec
+    // Next step (in development): produce vec of structs from input vec.
+    // Eventual plan: count total_orbits as create vec of structs, using the orbits value of
+    // previously added structs as data required when the chain of heirarchy in the input list jumps
+    // to a body (struct) that is located more than 1 item previous in the list.
     
+    // This is just a POC adding structs to a vec (which can then be read)
     let mut bodies: Vec<Body> = vec![];
 
     for i in 0..5 {
@@ -43,9 +47,9 @@ fn main() {
         bodies.push(body);
     }
 
-    // for elem in my_vec {
-    //     elem.print();
-    //     println!("name: {}", elem.get_name());
-    // }
-    
+    // DEBUG:
+    // for body in bodies {
+    //     body.print();
+    //     println!("name: {}", body.get_name());
+    // } 
 }
