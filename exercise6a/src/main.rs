5bd22650c411861a40e8b1f0923428a827989498
Simon Tharby <jinjamail@gmail.com>
2020-10-28 01:56:30 +0000

    Ex. 6a refactor parsing of input txt to reduce code complexity

--- exercise6a/src/main.rs
+++ exercise6a/src/main.rs
@@ -18,33 +18,29 @@ impl Body<'_> {
 
 fn main() {
     // get vec of components from input
-    let memory_string: String = fs::read_to_string("input.txt").expect("Error reading file!");
-    let split_input: Vec<&str> = memory_string.split('\n').collect();
+    let input_string: String = fs::read_to_string("input.txt").expect("Error reading file!");
+    let split_input: Vec<&str> = input_string.split('\n').collect();
     let mut input: Vec<&str> = vec![];
-    let mut index: usize = 0;
 
-    for _elem in &split_input {
-        let new_elem: Vec<&str> = split_at_parenthesis(&split_input[index]);
-        input.push(new_elem[0]);
-        input.push(new_elem[1]);
-        index += 1;
+    for elem in &split_input {
+        let mut new_elem: Vec<&str> = elem.split(")").collect();
+        input.append(&mut new_elem);
     }
 
-    println!("input vec: {:?}", input);
+    println!("input vec: {:?}", input); // DEBUG
 
-
-
+    // next step (in development): produce vec of structs from input vec
     
-    let mut my_vec: Vec<Body> = vec![];
+    let mut bodies: Vec<Body> = vec![];
 
     for i in 0..5 {
-        let my_struct = Body {
+        let body = Body {
             name: "some_name",
             orbits: i,
             orbiting: "some_name", 
         };
 
-        my_vec.push(my_struct);
+        bodies.push(body);
     }
 
     // for elem in my_vec {
@@ -52,14 +48,4 @@ fn main() {
     //     println!("name: {}", elem.get_name());
     // }
     
-}
-
-fn split_at_parenthesis(string: &str) -> Vec<&str> {
-    let mut list = vec![];
-
-    for elem in string.split(")").filter(|&x| !x.is_empty()) {
-        list.push(elem);
-    }
-
-    list
 }
